8bf06d6a5643fbc4c9ac65e485264780743bae65
Dat Nguyen <datnpt2004@gmail.com>
2024-03-25 15:25:59 +0700

    Ex3 Finished

--- Lab 2/WinFormsApp1/WinFormsApp1/EX3.cs
+++ Lab 2/WinFormsApp1/WinFormsApp1/EX3.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace WinFormsApp1
@@ -30,7 +32,7 @@ namespace WinFormsApp1
             string path = filePathInput.Text;
             if (string.IsNullOrEmpty(path) || !IsValidPath(path))
             {
-                MessageBox.Show("Invalid path.");
+                MessageBox.Show("Đường dẫn không phù hợp");
             }
             else
             {
@@ -62,84 +64,116 @@ namespace WinFormsApp1
                     writer.WriteLine($"{line} = {result}");
                 }
             }
-            MessageBox.Show("Content has been saved to a new file.");
+            MessageBox.Show("Kết quả đã được lưu vào tập tin mới");
         }
 
         private long EvaluateExpression(string expression)
         {
-            // Remove all spaces from the expression
             expression = expression.Replace(" ", "");
-
             return EvaluateExpressionHelper(expression);
         }
 
         private long EvaluateExpressionHelper(string expression)
         {
-            // Find the innermost bracketed expression
-            int startBracketIndex = expression.LastIndexOf('(');
-            if (startBracketIndex != -1)
+            Stack<int> stack = new Stack<int>();
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+
+            for (int i = 0; i < expression.Length; i++)
             {
-                int endBracketIndex = expression.IndexOf(')', startBracketIndex);
-                if (endBracketIndex == -1)
-                    throw new ArgumentException("Invalid expression: Unmatched brackets");
-
-                // Evaluate the expression inside the brackets recursively
-                string subExpression = expression.Substring(startBracketIndex + 1, endBracketIndex - startBracketIndex - 1);
-                long subResult = EvaluateExpressionHelper(subExpression);
-
-                // Replace the bracketed expression with its result
-                expression = expression.Substring(0, startBracketIndex) + subResult + expression.Substring(endBracketIndex + 1);
-                return EvaluateExpressionHelper(expression); // Recur to handle nested brackets
-            }
-            else
-            {
-                // If no brackets found, evaluate the entire expression without recursion
-                string[] tokens = expression.Split(new char[] { '+', '-', '*', '/' });
-
-                long total = 0;
-                char currentOperator = '+';
-
-                foreach (string token in tokens)
+                if (expression[i] == '(')
                 {
-                    if (IsOperator(token[0])) // Check if the first character of the token is an operator
-                    {
-                        currentOperator = token[0];
-                    }
-                    else
-                    {
-                        long nextNumber;
-                        if (!long.TryParse(token, out nextNumber))
-                            throw new FormatException("Invalid number format");
-
-                        switch (currentOperator)
-                        {
-                            case '+':
-                                total += nextNumber;
-                                break;
-                            case '-':
-                                total -= nextNumber;
-                                break;
-                            case '*':
-                                total *= nextNumber;
-                                break;
-                            case '/':
-                                if (nextNumber == 0)
-                                    throw new DivideByZeroException("Division by zero.");
-                                total /= nextNumber;
-                                break;
-                            default:
-                                throw new ArgumentException("Invalid operator");
-                        }
-                    }
+                    stack.Push(i);
                 }
+                else if (expression[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        MessageBox.Show("Input format không hợp lệ");
+                        return 0;
+                    }
 
-                return total;
+                    pairs.Add(stack.Pop(), i);
+                }
             }
+
+            if (stack.Count != 0)
+            {
+                MessageBox.Show("Input format không hợp lệ");
+                return 0;
+            }
+
+            return Evaluate(expression, 0, expression.Length - 1, pairs);
         }
 
-        private bool IsOperator(char c)
+        private long Evaluate(string expression, int start, int end, Dictionary<int, int> pairs)
         {
-            return c == '+' || c == '-' || c == '*' || c == '/';
+            if (start > end)
+                return 0;
+
+            if (expression[start] == '(' && pairs.ContainsKey(start) && pairs[start] == end)
+                return Evaluate(expression, start + 1, end - 1, pairs);
+
+            var operators = new char[] { '+', '-', '*', '/' };
+            var tokens = SplitExpression(expression.Substring(start, end - start + 1), operators);
+
+            var values = new List<long>();
+            var ops = new List<char>();
+
+            foreach (var token in tokens)
+            {
+                if (token == "+" || token == "-" || token == "*" || token == "/")
+                {
+                    ops.Add(token[0]);
+                }
+                else
+                {
+                    values.Add(long.Parse(token));
+                }
+            }
+
+            for (int i = 0; i < ops.Count; i++)
+            {
+                if (ops[i] == '*' || ops[i] == '/')
+                {
+                    long temp = ops[i] == '*' ? values[i] * values[i + 1] : values[i] / values[i + 1];
+                    values[i] = temp;
+                    values.RemoveAt(i + 1);
+                    ops.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            long result = values[0];
+            for (int i = 0; i < ops.Count; i++)
+            {
+                if (ops[i] == '+')
+                {
+                    result += values[i + 1];
+                }
+                else if (ops[i] == '-')
+                {
+                    result -= values[i + 1];
+                }
+            }
+
+            return result;
+        }
+
+        private List<string> SplitExpression(string expression, char[] operators)
+        {
+            var result = new List<string>();
+            int startIndex = 0;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (operators.Contains(expression[i]))
+                {
+                    result.Add(expression.Substring(startIndex, i - startIndex));
+                    result.Add(expression[i].ToString());
+                    startIndex = i + 1;
+                }
+            }
+            result.Add(expression.Substring(startIndex));
+            return result;
         }
     }
 }
